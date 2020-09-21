7af3cd1f172e64317eaaf574d081191fe02a0eb2
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-09-21 22:01:44 +0200

    added cgmath, first step

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,29 @@ boid
 Its purpose is to implement flocking behaviour.
 */
 
+extern crate cgmath;
+
+/// The data structure to store relevant information
+/// for each boid.
+#[derive(Debug)]
+struct Boid {
+    position: cgmath::Vector3<f64>,
+}
+
+impl Boid {
+    /// Create a new boid.
+    fn new() -> Boid {
+        return Boid {
+            position: cgmath::Vector3::new(1f64, 2f64, 3f64),
+        };
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new() {
+        let b = crate::Boid::new();
+        assert_eq!(b.position, cgmath::vec3(1f64, 2f64, 3f64));
     }
 }
