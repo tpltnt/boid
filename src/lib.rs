f9ee4e98539e007c31525469840dd4b66ed1c3d2
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-09-21 21:34:41 +0200

    basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+/*!
+boid
+====
+**boid** is a 2 and 3-dimensional boid library written in the Rust programming language.
+Its purpose is to implement flocking behaviour.
+*/
+
 #[cfg(test)]
 mod tests {
     #[test]
